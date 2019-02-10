9c3f24aa8a9fd532995fd5119fd767e10977a500
Steven Cardona <cardona.sj@gmail.com>
2019-02-10 13:39:32 -0600

    Add shop tour page

--- assets/js/shopss.js
+++ assets/js/shopss.js
@@ -1,10 +1,8 @@
-var slideIndex = [1,1,1,1,1];
+var slideIndex = [1];
 /* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides", "mySlides1", "mySlides2" ,"mySlides3"] 
+var slideId = ["mySlides4"] 
 showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
+
 
 function plusSlides(n,no) {
   showSlides(slideIndex[no] += n, no);
@@ -18,5 +16,5 @@ function showSlides(n, no) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none"; 
   }
-  x[slideIndex[no]-1].style.display = "block"; 
+  x[slideIndex[no]-1].style.display = "block";
 }
